94caba8f631abdd0935d8ce6371abda71a0b0764
Erik Jenks <erikjenks@gmail.com>
2015-02-23 07:36:47 -0500

    added search

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,8 @@ class Restaurant < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	has_many :reviews
 
+	searchkick
+
 	validates :name, :address, :phone, :website, :image, presence: :true
 	validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
   message: "must be in the format (123) 456-7890" }
@@ -9,5 +11,6 @@ class Restaurant < ActiveRecord::Base
   		message: "must start with http:// or https://" }
 	validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-z]{2} \d{5}\z/,
 		message: "must be in the format of 350 Fifth Avenue, New York, NY 10118" }	
+
 
 end
